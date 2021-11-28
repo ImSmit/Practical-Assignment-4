944d0039aca7e9d285a818915834afc42c7107f1
smit patel <59001731+ImSmit@users.noreply.github.com>
2021-11-28 18:24:46 +0530

    Add files via upload

---
+++ routs/company.js
@@ -0,0 +1,6 @@
+const company = [{
+    company_id : "1",
+    name : "samsung",
+    product_ids : "1"
+}]
+module.exports = company;
---
+++ routs/seller.js
@@ -0,0 +1,6 @@
+const seller = [{
+    seller_id : "1",
+    name : "bhatia",
+    product_ids: ""
+}]
+module.exports = seller;
